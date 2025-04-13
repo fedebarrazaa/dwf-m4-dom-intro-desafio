bf062c2d5bfe42e070af94947e2901b88a1b70ec
fedebarrazaa <fedebarraza97@gmail.com>
2025-04-13 16:29:04 -0300

    cambios en listas

--- index.js
+++ index.js
@@ -25,6 +25,31 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector(".lista");
+  const ItemsEls = document.querySelectorAll(".lista li");
+
+  //for of para remover la lista actual
+  for (let el of ItemsEls) {
+    el.remove()
+  }
+
+  //for of para crear los nuevos 
+  for (let item of cosasQueAprendimos) {
+    const newLi = document.createElement('li');
+    newLi.textContent = item.tema;
+
+    const claseQueVoyAsignar = item.class || "item"; //poner item en las clases vacias
+    newLi.classList.add(claseQueVoyAsignar); //asignar ya las clases que tienen el array
+
+    listaEl.appendChild(newLi)
+    
+  }
+
+
+  
+
+  
+}
 
 main();
